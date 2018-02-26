64b1318fbd826de6657b8a0216816d882c39b145
Exidex <mrexidex@gmail.com>
2018-02-26 21:32:29 +0100

    Initial commit

---
+++ src/main/java/com/wspa/mpkmanager/MpkManagerApplication.java
@@ -0,0 +1,12 @@
+package com.wspa.mpkmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpkManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpkManagerApplication.class, args);
+	}
+}
